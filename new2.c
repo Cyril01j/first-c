8d9bb841e1ca1cc069e7bf48f786a79bb7f1d8cf
Cyril Jaiswal <163710599+Cyril01j@users.noreply.github.com>
2024-08-18 10:11:55 +0530

    Add files via upload

---
+++ new2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+    {
+        // Creating variables having different data types
+       int integer=2;
+       float floating=29.69;
+       char character='a';
+
+        // Printing variables with the help of their respective format specifiers
+        printf("%d\n", integer);
+        printf("%f\n", floating);
+        printf("%c\n", character);
+        return 0;
+    }
