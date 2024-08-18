1f4b2ee7cdf35d8e2e51a275685afe63404ac9f9
Cyril Jaiswal <163710599+Cyril01j@users.noreply.github.com>
2024-08-18 10:20:51 +0530

    Add files via upload

--- new.c
+++ new.c
@@ -1,23 +1,27 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
-    {
-        int a=20, b=60, c;
-        printf("hello world\n");
-        
-        c = b - a;
-        printf("The value of b - a\n");
-        printf("%d\n\n",c);
-        
-        c = b + a;
-        printf("The value of b + a\n");
-        printf("%d\n\n",c);
+{
+    int a = 10, b = 60, c;
+    printf("hello world\n");
 
-        c = b * a;
-        printf("The value of b * a\n");
-        printf("%d\n\n",c);
+    c = b - a;
+    printf("The value of b - a\n");
+    printf("%d\n\n", c);
 
-        c = b / a;
-        printf("The value of b / a\n");
-        printf("%d",c);
-        return 0;
-    }
+    c = b + a;
+    printf("The value of b + a\n");
+    printf("%d\n\n", c);
+
+    c = b * a;
+    printf("The value of b * a\n");
+    printf("%d\n\n", c);
+
+    c = b / a;
+    printf("The value of b / a\n");
+    printf("%d\n", c);
+
+    c = b % a;
+    printf("The value of modulus \n");
+    printf("%d", c);
+    return 0;
+}
